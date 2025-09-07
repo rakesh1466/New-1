af63f154e4960ab05280d1c4bb06dc776019e9c5
S.Rakesh <rakeshsennappan@gmail.com>
2025-09-07 22:49:19 +0530

    Add files via upload

---
+++ distance btwn 2 points.py
@@ -0,0 +1,12 @@
+# Python3 program to calculate 
+# distance between two points
+
+import math
+
+# Function to calculate distance
+def distance(x1 , y1 , x2 , y2):
+    return math.sqrt(math.pow(x2 - x1, 2) +
+                math.pow(y2 - y1, 2))
+
+# Drivers Code
+print("%.6f"%distance(3, 4, 4, 3))
---
+++ palindrome.py
@@ -0,0 +1,16 @@
+s = input()  # string
+
+i,j = 0, len(s) - 1  # two pointers
+
+is_palindrome = True  # assume palindrome
+while i < j:
+    if s[i] != s[j]:  # mismatch found
+        is_palindrome = False
+        break
+    i += 1
+    j -= 1
+
+if is_palindrome:
+    print("Yes") 
+else:
+    print("No")
